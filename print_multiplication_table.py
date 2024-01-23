2e820be5691c2404d1a9e9f6b70be6a3462ebd0b
Jossel Redoquerio <josselgredoquerio@iskolarngbayan.pup.edu.ph>
2024-01-23 13:50:40 +0800

    print label and spacing

--- print_multiplication_table.py
+++ print_multiplication_table.py
@@ -1,11 +1,18 @@
 # Exercise 13: Print multiplication table from 1 to 10
 
+print("\n")
+print("multiplication table 1 to 10:")
 for x in range(1, 11):
     for y in range(1, 11):
         print(x * y, end= " ")
-    print("\n")
+    print(" ")
 
+print("\n")
+
+print("multiplication table 1 to 20:")
 for a in range(1, 21):
     for b in range(1, 21):
         print(a * b, end= " ")
-    print("\n")
+    print("")
+
+print("\n")
